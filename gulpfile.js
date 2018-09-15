6f35e8eca4400755e2d5df2b534a2524b86454ef
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-09-15 15:52:43 +0700

    upd styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
-// Sass configuration
 var gulp = require('gulp');
+var path = require('path');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var open = require('gulp-open');
 
-gulp.task('sass', function() {
-    gulp.src('*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
+var Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './assets/css/',
+  SCSS_TOOLKIT_SOURCES: './assets/scss/portal.white-sd.scss',
+  SCSS: './assets/scss/**/**'
+};
+
+gulp.task('compile-scss', function() {
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
 });
 
-gulp.task('default', ['sass'], function() {
-    gulp.watch('*.scss', ['sass']);
-})
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, ['compile-scss']);
+});
+
+gulp.task('open', function() {
+  gulp.src('examples/index.html')
+    .pipe(open());
+});
+
+gulp.task('open-app', ['open', 'watch']);

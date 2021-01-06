1d3307ed0c930d3db714a1b822fe4607edcf5431
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-01-06 19:01:26 +0700

    upd bs layout

--- src/assets/js/mix-fe-portal.js
+++ src/assets/js/mix-fe-portal.js
@@ -91,13 +91,3 @@
 //     // });
 // });
 
-
-var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
-triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', function (event) {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
